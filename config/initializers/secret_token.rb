746cca02e12cbd857b9d20ad41b8952648933626
Eric Kelly <heroiceric@gmail.com>
2014-01-19 12:23:02 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OpalPlay::Application.config.secret_key_base = '7fbb0248f7a9757ec8a9f9c946a2dad1faa1c29f17c8e823b8d1d29a7b1f1ca88faa8bcd4be71a6a013a7f3d3aa22a52cd6ce75e6941fbe997c6c03bbdf61d3d'
